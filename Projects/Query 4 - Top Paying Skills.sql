fc2745a4266dad9aadc7582610296fc47b67ed0b
Victor <nguyenthang8856@gmail.com>
2024-12-24 11:04:34 +0700

    Query 4

---
+++ Projects/Query 4 - Top Paying Skills.sql
@@ -0,0 +1,14 @@
+SELECT
+    skills,
+    AVG(salary_year_avg) AS avg_salary_skill
+FROM
+    skills_dim
+INNER JOIN skills_job_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+INNER JOIN job_postings_fact ON skills_job_dim.job_id = job_postings_fact.job_id
+WHERE
+    salary_year_avg IS NOT NULL
+    AND job_title_short = 'Data Analyst'
+GROUP BY
+    skills
+ORDER BY
+    avg_salary_skill DESC
